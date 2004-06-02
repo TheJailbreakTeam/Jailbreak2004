2c2f4950a845591251df4c0e700db1699d3f99df
mychaeel <>
2004-06-02 13:05:27 +0000

    make-distribution-update.bat * Added some blank output lines for aesthetics.

--- Jailbreak/Installer/make-distribution-update.bat
+++ Jailbreak/Installer/make-distribution-update.bat
@@ -3,14 +3,17 @@
 echo ===============================================================================
 echo Full Installer
 echo ===============================================================================
+echo.
 perl make-distribution.pl --version="a" --skip-keypress
 
 if errorlevel 1 goto :END
 
 echo.
+echo.
 echo ===============================================================================
 echo Patch
 echo ===============================================================================
+echo.
 perl make-distribution.pl --version="a-Patch" --reference-file="Jailbreak2004-reference.txt" --reference-version=100 --skip-rebuild
 
 
