f096a88ceaa8f793613d53d9a4e82efa1973ace3
wormbo <>
2007-04-29 10:31:03 +0000

    Jailbreak2004b-zip-reference.txt * added file
    make-distribution-update.bat
    * uses Jailbreak2004b-zip-reference.txt and version 102 as reference now
    * creates ZIP archives for the patch instead of 7z

--- Jailbreak/Installer/make-distribution-update.bat
+++ Jailbreak/Installer/make-distribution-update.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo ===============================================================================
-echo Full Installer
+echo Full Installer (zip)
 echo ===============================================================================
 echo.
 perl make-distribution.pl --version="c" --skip-keypress --zip zip
@@ -14,7 +14,7 @@ echo ===========================================================================
 echo Patch
 echo ===============================================================================
 echo.
-perl make-distribution.pl --version="c-Patch" --reference-file="Jailbreak2004-reference.txt" --reference-version=100 --skip-rebuild --zip 7z
+perl make-distribution.pl --version="c-Patch" --reference-file="Jailbreak2004b-reference.txt" --reference-version=102 --skip-rebuild --zip zip
 
 
 :END
