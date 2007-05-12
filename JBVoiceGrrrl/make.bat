abbfcc01c60b191de6609a5f1a9273d8deadbd92
wormbo <>
2007-05-12 12:38:45 +0000

    make.bat * backs up old package now, if it exists * conforms new package to backed-up version, if it exists

--- JBVoiceGrrrl/make.bat
+++ JBVoiceGrrrl/make.bat
@@ -7,7 +7,10 @@
 echo Creating package...
 
 cd ..\System
+if exist ..\Sounds\JBVoiceGrrrl.uax.backup del ..\Sounds\JBVoiceGrrrl.uax.backup
+if exist ..\Sounds\JBVoiceGrrrl.uax ren ..\Sounds\JBVoiceGrrrl.uax JBVoiceGrrrl.uax.backup
 ucc pkg import sound ..\Sounds\JBVoiceGrrrl ..\JBVoiceGrrrl\Sounds
+if exist ..\Sounds\JBVoiceGrrrl.uax.backup ucc conform ..\Sounds\JBVoiceGrrrl.uax ..\Sounds\JBVoiceGrrrl.uax.backup
 
 echo Done.
 
